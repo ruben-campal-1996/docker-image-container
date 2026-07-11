350d74789562a2a1a4f13f8bf0bee29efd53d62f
Rubén Campal <ruben.campal.lopez1996@gmail.com>
2026-07-11 14:06:36 +0200

    add SQL TABLE and DATA

---
+++ insert_pets.sql
@@ -0,0 +1,6 @@
+INSERT INTO pets(name, animal_type, race, age)
+VALUE
+('Nube', 'Cat', 'European', 3),
+('Osito', 'Dog', 'Golden Retrieber', 7),
+('Paca','Bear','Cantabric', 13),
+('Tola','Bear','Cantabric', 14);
---
+++ table_pets.sql
@@ -0,0 +1,7 @@
+CREATE TABLE pets (
+    id_pet INTEGER PRIMARY KEY,
+    name VARCHAR(50),
+    animal_type CHAR(50),
+    race CHAR(50),
+    age INTEGER
+)
